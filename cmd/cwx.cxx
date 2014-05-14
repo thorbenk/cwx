8049d2cc970a8ea7044aace4c699b40caa673185
Thorben Kroeger <thorben.kroeger@iwr.uni-heidelberg.de>
2014-05-14 10:30:17 +0200

    reorganize test files
    - unittests go into top-level 'test'
    - command line utilities go into top-level 'cmd'

---
+++ cmd/cwx.cxx
@@ -0,0 +1,45 @@
+#include <string>
+#include <random>
+#include <fstream>
+
+#include "andres/marray_hdf5.hxx"
+#include "cwx/cwx.hxx"
+#include "cwx/sketch.hxx"
+
+inline void test(const bool& pred) {
+    if(!pred) throw std::runtime_error("Test failed.");
+}
+
+int main(int argc, char **argv) {
+    using namespace andres;
+    
+    if(argc != 3) {
+        std::cerr << "Parameters: <input-hdf5-file> <input-dataset>" << std::endl;
+        return 1;
+    }
+    std::string fileName = argv[1];
+    std::string datasetName = argv[2];
+
+    typedef unsigned int Label;
+    typedef unsigned int Coordinate;
+
+    // load volume labeling
+    Marray<Label> volumeLabeling;
+    hid_t file = hdf5::openFile(fileName);
+    hdf5::load(file, datasetName, volumeLabeling);
+    hdf5::closeFile(file);
+
+    // build CWX data structure
+    cwx::CWX<Label, Coordinate> cwx;
+    cwx.build(volumeLabeling, true);
+    
+    // TODO: add tests here
+    
+    hid_t outFile(hdf5::createFile("out.h5"));
+    hdf5::save(outFile, "cwx", cwx.grid());
+    hdf5::closeFile(outFile);
+
+    return 0;
+}
+
+
